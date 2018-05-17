bd9d70fad4f5d1bbde0d6d22a57cc4c1b490470f
qu.zheqi <qu.zheqi@ztesoft.com>
2018-05-17 19:18:34 +0800

    先处理一次gitignore

--- forum/data/sysdata/cache_pluginsetting.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Discuz! cache file, DO NOT modify me!
-//Identify: 90dae891619d3c35915167a745d4ca03
-
-$pluginsetting = array (
-);
-?>
--- forum/uc_server/data/logs/201805.php
+++
@@ -1,1 +0,0 @@
-<?PHP exit;?>	admin	183.208.23.157	1526036457	login	succeed
